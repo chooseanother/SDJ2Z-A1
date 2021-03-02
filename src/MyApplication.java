b3b169cdd8ae463f8729c8ee23393f21dee0f552
Luka69-420 <lukaudovicic10@gmail.com>
2021-03-02 19:09:46 +0100

    MyApplication class is updated with Threads

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,3 +1,4 @@
+import external.Thermometer;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import mediator.Model;
@@ -6,11 +7,34 @@ import view.ViewHandler;
 import viewmodel.ViewModelFactory;
 
 public class MyApplication extends Application {
+    Thread t1;
+    Thread t2;
+
     public void start(Stage primaryStage) {
+
+        // Model
+
         Model model = new ModelManager();
         ViewModelFactory viewModelFactory = new ViewModelFactory(model);
-        ViewHandler view = new ViewHandler(viewModelFactory);
 
+        t1 = new Thread(new Thermometer(model, "t1", Math.random(), 1));
+        t2 = new Thread(new Thermometer(model, "t2", Math.random(), 2));
+
+        t1.start();
+        t2.start();
+
+        // View
+
+        ViewHandler view = new ViewHandler(viewModelFactory);
         view.start(primaryStage);
     }
+
+    @Override
+    public void stop()
+    {
+        t1.stop();
+        t2.stop();
+    }
+
+
 }
