2fe3566954caf4ee3651e06def9779f09ca10300
david <michalcodavid@gmail.com>
2021-03-02 20:32:29 +0100

    Updated

--- src/viewmodel/HistoryViewModel.java
+++ src/viewmodel/HistoryViewModel.java
@@ -1,8 +1,12 @@
 package viewmodel;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import mediator.Model;
+import model.Temperature;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -10,15 +14,21 @@ import java.beans.PropertyChangeListener;
 public class HistoryViewModel implements PropertyChangeListener {
     private Model model;
     private ObservableList<SimpleTemperatureViewModel> list;
+    private ObjectProperty<SimpleTemperatureViewModel> selectedExerciseProperty;
+
 
     public HistoryViewModel(Model model){
         this.model = model;
+        this.model.addListener(this);
         this.list = FXCollections.observableArrayList();
+        selectedExerciseProperty = new SimpleObjectProperty<>();
     }
-
 
     public void clear(){
         list.clear();
+    }
+    public void setSelected(SimpleTemperatureViewModel exerciseVM){
+        selectedExerciseProperty = new SimpleObjectProperty<>(exerciseVM);
     }
 
     public ObservableList<SimpleTemperatureViewModel> getAll(){
@@ -26,11 +36,11 @@ public class HistoryViewModel implements PropertyChangeListener {
     }
 
     @Override public void propertyChange(PropertyChangeEvent evt) {
-        if (evt.getPropertyName().equals("add")) {
-            if (list.size() > 20) {
+        if (evt.getPropertyName().equals("temperature")) {
+            if (list.size() >= 20) {
                 list.remove(list.size() - 1);
             }
-            list.add(new SimpleTemperatureViewModel(model.getLastInsertedTemperature()));
+            list.add(0,new SimpleTemperatureViewModel(model.getLastInsertedTemperature()));
         }
 
     }
