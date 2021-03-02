a768acda78ec43f831fcee6b437c76e55d85a19d
hanch <jurco10510@gmail.com>
2021-03-02 16:29:49 +0100

    SimpleTemperatureViewModel constructor,getters

--- src/viewmodel/SimpleTemperatureViewModel.java
+++ src/viewmodel/SimpleTemperatureViewModel.java
@@ -1,8 +1,24 @@
 package viewmodel;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import model.Temperature;
 
 public class SimpleTemperatureViewModel {
-  private StringProperty ID;
-  private double value;
+  private StringProperty IDProperty;
+  private DoubleProperty valueProperty;
+
+  public SimpleTemperatureViewModel(Temperature temperature){
+    IDProperty = new SimpleStringProperty(temperature.getId());
+    valueProperty = new SimpleDoubleProperty(temperature.getValue());
+  }
+
+  public StringProperty getIDProperty(){
+    return IDProperty;
+  }
+  public DoubleProperty getValueProperty(){
+    return valueProperty;
+  }
 }
