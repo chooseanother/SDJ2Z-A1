89e924eebf49f3d2fe23d37cf2a04a03261a086c
chooseanother <kim.tranberg@gmail.com>
2021-03-02 09:54:47 +0100

    Created files

---
+++ src/view/LimitViewController.java
@@ -0,0 +1,29 @@
+package view;
+
+import javafx.scene.layout.Region;
+import viewmodel.LimitViewModel;
+import viewmodel.LogViewModel;
+
+public class LimitViewController {
+    private ViewHandler viewHandler;
+    private LimitViewModel viewModel;
+    private Region root;
+
+    public LimitViewController(){}
+
+    public void init(ViewHandler viewHandler, LimitViewModel viewModel, Region root) {
+        this.viewHandler = viewHandler;
+        this.viewModel = viewModel;
+        this.root = root;
+
+        //bind
+    }
+
+    public Region getRoot(){
+        return root;
+    }
+
+    public void reset(){
+        viewModel.clear();
+    }
+}
---
+++ src/viewmodel/LimitViewModel.java
@@ -0,0 +1,14 @@
+package viewmodel;
+
+import model.Model;
+
+public class LimitViewModel {
+    private Model model;
+
+    public LimitViewModel(Model model){
+        this.model = model;
+    }
+
+    public void clear(){
+    }
+}
