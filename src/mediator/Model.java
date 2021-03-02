5b2332cb352ef2c4e80f21e53b6767a1c6312a53
Luka69-420 <lukaudovicic10@gmail.com>
2021-03-02 16:35:25 +0100

    Added heater instace variable in modelmanager , 2 methods in modelmanger heatup and heatdown

--- src/mediator/Model.java
+++ src/mediator/Model.java
@@ -2,7 +2,10 @@ package mediator;
 
 import model.Temperature;
 
-public interface Model extends UnnamedPropertyChangeSubject {
+public interface Model extends UnnamedPropertyChangeSubject
+{
+    void heatUp();
+    void heatDown();
     void addTemperature(String id, double temperature);
     Temperature getLastInsertedTemperature();
     Temperature getLastInsertedTemperature(String id);
