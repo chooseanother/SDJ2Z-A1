98a967b6c47ddd146fe3bffba48256dfe657082d
david <michalcodavid@gmail.com>
2021-03-02 15:36:58 +0100

    Updated LogViewModel

--- src/viewmodel/LogViewModel.java
+++ src/viewmodel/LogViewModel.java
@@ -2,15 +2,32 @@ package viewmodel;
 
 
 import model.Model;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class LogViewModel {
+public class LogViewModel implements  PropertyChangeListener  {
     private Model model;
+    private ObservableList<String> log;
 
-    public LogViewModel(Model model){
+    public LogViewModel(Model model) {
         this.model = model;
+        log = FXCollections.observableArrayList();
 
     }
 
+    public ObservableList<String> getLog() {
+        return log;
+    }
+
     public void clear(){
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        Platform.runLater(()->log.add(0,evt.getOldValue()+": "+evt.getNewValue()));
     }
 }
