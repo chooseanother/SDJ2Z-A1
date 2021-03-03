3fcea34b51a63f9bd90731dc09e67db7658240dc
chooseanother <kim.tranberg@gmail.com>
2021-03-03 10:04:26 +0100

    Redid thermometers and threads, also added outdoor

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,3 +1,4 @@
+import external.OutdoorThermometer;
 import external.Thermometer;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -7,34 +8,38 @@ import view.ViewHandler;
 import viewmodel.ViewModelFactory;
 
 public class MyApplication extends Application {
-    Thread t1;
-    Thread t2;
+    OutdoorThermometer t0;
+    Thermometer t1;
+    Thermometer t2;
 
     public void start(Stage primaryStage) {
 
         // Model
-
         Model model = new ModelManager();
+
+        // View model
         ViewModelFactory viewModelFactory = new ViewModelFactory(model);
 
-        t1 = new Thread(new Thermometer(model, "t1", 0, 1));
-        t2 = new Thread(new Thermometer(model, "t2", 0, 7));
-
-        t1.start();
-        t2.start();
-
         // View
-
         ViewHandler view = new ViewHandler(viewModelFactory);
+
         view.start(primaryStage);
+
+        t0 = new OutdoorThermometer(model, "t0",-10,30);
+        t1 = new Thermometer(model, "t1", 0, 1);
+        t2 = new Thermometer(model, "t2", 0, 7);
+        Thread tt0 = new Thread(t0,"t0");
+        Thread tt1 = new Thread(t1,"t1");
+        Thread tt2 = new Thread(t2,"t2");
+        tt0.start();
+        tt1.start();
+        tt2.start();
     }
 
     @Override
-    public void stop()
-    {
+    public void stop() {
+        t0.stop();
         t1.stop();
         t2.stop();
     }
-
-
 }
